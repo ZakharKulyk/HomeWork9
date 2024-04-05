3aae99986ca2d9ad1cc039486df9549dbc70308f
zkhr <zakhar864@gmail.com>
2024-04-05 15:26:58 +0200

    added some this.

--- MyArrListTest.java
+++ MyArrListTest.java
@@ -5,15 +5,18 @@ import java.util.Arrays;
 public class MyArrListTest {
     public static void main(String[] args) {
         MyArrayList<Integer> myArrayList = new MyArrayList<>();
-        myArrayList.add(5);
-        myArrayList.add(7);
-        myArrayList.add(10);
+
+
+        for (int i = 0; i < 10; i++) {
+            myArrayList.add(i);
+        }
         System.out.println(myArrayList);
         myArrayList.clear();
         System.out.println(myArrayList);
-
-
-
+        Integer integer = myArrayList.get(0);
+        Integer remove = myArrayList.remove(1);
+        System.out.println(integer);
+        System.out.println(remove);
 
 
     }
