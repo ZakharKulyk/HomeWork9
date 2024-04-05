85c6020dc7391e3b2d2ee17dd9fc0e009471e9aa
zkhr <zakhar864@gmail.com>
2024-04-05 15:26:50 +0200

    added some this.

--- MyArrayList.java
+++ MyArrayList.java
@@ -3,15 +3,16 @@ package ua.goit.polymorpism.Module9.HomeWork9;
 import java.util.Arrays;
 
 public class MyArrayList<T> {
-    private T elements[];
-    private  int size;
+    private T[] elements;
+    private int size;
     private final int DEFAULT_SIZE = 10;
-    public MyArrayList(){
+
+    public MyArrayList() {
         this.elements = (T[]) new Object[DEFAULT_SIZE];
     }
 
     public T[] getElements() {
-        return elements;
+        return this.elements;
     }
 
     public void setElements(T[] elements) {
@@ -19,46 +20,53 @@ public class MyArrayList<T> {
     }
 
     public void add(T value) {
-        if (this.size == elements.length) {
+        if (this.size == this.elements.length) {
             resize();
         }
         this.elements[size] = value;
-        size++;
+        this.size++;
+
     }
-    public T remove(int index){
+
+    public T remove(int index) {
         T temp = null;
-        for(int i =index; i<size-1; i++)
-        {
-            temp = elements[index];
-            this.elements[index] = elements[i+1];
-
+        if (index >= 0 && index < this.size) {
+            temp = this.elements[index];
+            for (int i = index; i < this.size - 1; i++) {
+                this.elements[i] = this.elements[i + 1];
+            }
+            this.size--;
         }
-        this.size--;
-        return  temp;
-
-    }
-    public T get(int index){
-        return  this.elements[index];
+        return temp;
     }
 
-    public void clear()
-    {
-        Arrays.setAll(this.elements, i->0);
-        Arrays.setAll(this.elements, i->null);
+    public T get(int index) {
+        if (index >= 0 && index < size) {
+            return this.elements[index];
+        } else {
+            IndexOutOfBoundsException indexOutOfBoundsException = new IndexOutOfBoundsException();
+            throw indexOutOfBoundsException;
+        }
+
+    }
+
+    public void clear() {
+        Arrays.setAll(this.elements, i -> 0);
+        Arrays.setAll(this.elements, i -> null);
         this.size = 0;
     }
-    public int size()
-    {
-      return  this.size;
+
+    public int size() {
+        return this.size;
     }
 
-    public void resize(){
-        this.elements = (T[]) new Object[DEFAULT_SIZE*2];
+    public void resize() {
+        this.elements = Arrays.copyOf(this.elements, this.elements.length * 2);
     }
 
     @Override
     public String toString() {
-        return Arrays.toString(Arrays.copyOf(elements, size));
+        return Arrays.toString(Arrays.copyOf(this.elements, this.size));
     }
 
 }
